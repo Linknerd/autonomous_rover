f8f38fcda6b5a4d2dc5de256f9a03fe95dc843d0
mruzdijic <158781015+mruzdijic@users.noreply.github.com>
2026-03-12 16:34:12 -0400

    Corrected serial issues

--- arduino/robot_controller/robot_controller.ino
+++ arduino/robot_controller/robot_controller.ino
@@ -1,33 +1,43 @@
 #include "movement.h"
 #include "sensors.h"
 
+static String serialBuffer = "";
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   movement_setup();
   initSensors();
   Serial.println("I,Program initialized.");
+}
+
+void processCommand(String cmd) {
+  cmd.trim();
+
+  if (cmd.startsWith("V,")) {
+    int firstComma  = cmd.indexOf(',');
+    int secondComma = cmd.indexOf(',', firstComma + 1);
+
+    if (firstComma != -1 && secondComma != -1) {
+      vd = cmd.substring(firstComma + 1, secondComma).toFloat();
+      wd = cmd.substring(secondComma + 1).toFloat();
+    }
+  } else if (cmd == "S") {
+    vd = 0.0;
+    wd = 0.0;
+  }
 }
 
 void loop() {
   updateSensors();
   PID();
 
-  if (Serial.available()) {
-    String cmd = Serial.readStringUntil('\n');
-    cmd.trim();
-
-    // Protocol: V,vd,wd\n
-    if (cmd.startsWith("V,")) {
-      int firstComma  = cmd.indexOf(',');
-      int secondComma = cmd.indexOf(',', firstComma + 1);
-
-      if (firstComma != -1 && secondComma != -1) {
-        vd = cmd.substring(firstComma + 1, secondComma).toFloat();
-        wd = cmd.substring(secondComma + 1).toFloat();
-      }
-    } else if (cmd == "S") {
-      vd = 0.0;
-      wd = 0.0;
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') {
+      processCommand(serialBuffer);
+      serialBuffer = "";
+    } else if (c != '\r') {
+      serialBuffer += c;
     }
   }
 }
